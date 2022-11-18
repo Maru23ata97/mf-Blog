dacbc466e3ae8254b491fdaeacd1a7a2a4b84d31
Maru23ata97 <118214748+Maru23ata97@users.noreply.github.com>
2022-11-18 13:43:02 -1000

    1 file modified

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const card = (title, description) => `
+const card = (title, description, image) => `
   <div class="col-lg-4 col-sm-12">
     <div class="card mb-5 shadow-sm">
-      <img src="https://picsum.photos/1000/600" class="img-fluid" />
+      <img src="${image}" class="img-fluid" />
 
       <div class="card-body">
         <div class="card-title">
@@ -9,7 +9,6 @@ const card = (title, description) => `
         </div>
         <div class="card-text">
           <p>${description}</p>
-        </div>
         <a href="#" class="btn btn-outline-info rounded-0 float-end">Read More</a>
       </div>
     </div>
@@ -21,7 +20,45 @@ const card = (title, description) => `
     'https://jsonplaceholder.typicode.com/posts'
   ).then((response) => response.json());
   console.log(apiData);
-  const myData = [{ title: 'Retourne moi', description: 'Donne moi' }];
+  const myData = [
+    {
+      title: 'Le monde du web',
+      description: 'Le monde du web est très vaste, car plusieurs langages est utilisées mais qui est complémentaire. HTML est le squelette, CSS est la peau, le muscle represente JavaScript',
+      image:
+        'https://www.photo-paysage.com/albums/userpics/10001/thumb_Les_rayons_du_soleil_a_travers_les_feuillages_dans_la_foret.JPG',
+    },
+    {
+      title: 'Le langage du HTML, CSS, JavaSccript',
+      description: "A la découverte des fonctionnalités des trois langages... j'ai ",
+      image:
+        'https://www.photo-paysage.com/albums/userpics/10001/thumb_Les_rayons_du_soleil_a_travers_les_feuillages_dans_la_foret.JPG',
+    },
+    {
+      title: 'La création template',
+      description: 'Découuverte et création template.... ',
+      image:
+        'https://www.photo-paysage.com/albums/userpics/10001/thumb_Les_rayons_du_soleil_a_travers_les_feuillages_dans_la_foret.JPG',
+    },
+    {
+      title: 'Création du PortFolio',
+      description: 'La création du Portfolio est ...',
+      image:
+        'https://www.photo-paysage.com/albums/userpics/10001/thumb_Les_rayons_du_soleil_a_travers_les_feuillages_dans_la_foret.JPG',
+    },
+    {
+      title: '',
+      description: 'Donne moi',
+      image:
+        'https://www.photo-paysage.com/albums/userpics/10001/thumb_Les_rayons_du_soleil_a_travers_les_feuillages_dans_la_foret.JPG',
+    },
+    {
+      title: 'Retourne moi',
+      description: 'Donne moi',
+      image:
+        'https://www.photo-paysage.com/albums/userpics/10001/thumb_Les_rayons_du_soleil_a_travers_les_feuillages_dans_la_foret.JPG',
+    },
+  ];
+
 
   let cardList = '';
 
@@ -29,7 +66,8 @@ const card = (title, description) => `
     console.log(post.title);
     document.getElementById('card-list').innerHTML += card(
       post.title,
-      post.description
+      post.description,
+      post.image
     );
   });
 })();
